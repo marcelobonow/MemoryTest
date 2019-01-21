4776e3a4593d00d56e5e4f3030267e457832a6e0
Marcelo Bonow <marcelobonowott@hotmail.com>
2019-01-21 03:07:45 -0200

    Código de teste

--- MemoryTest/Assets/Scripts/CardsManager.cs
+++ MemoryTest/Assets/Scripts/CardsManager.cs
@@ -9,15 +9,40 @@ public class CardsManager : MonoBehaviour
     [SerializeField] private GridLayoutGroup cardsHolder;
     private List<Card> cards;
 
+    private Card firstCardClick;
+
     private void Start()
     {
         cards = new List<Card>();
         var card = Instantiate(cardPrefab, cardsHolder.transform).GetComponent<Card>();
         card.AddOnClick(OnCardClicked);
+        card.SetCard(1, 1, null);
+        cards.Add(card);
+        card = Instantiate(cardPrefab, cardsHolder.transform).GetComponent<Card>();
+        card.AddOnClick(OnCardClicked);
+        card.SetCard(1, 1, null);
         cards.Add(card);
     }
     private void OnCardClicked(Card card)
     {
+        if(firstCardClick == null)
+        {
+            firstCardClick = card;
+            firstCardClick.AnimateFlipping();
+        }
+        else
+        {
+            card.AnimateFlipping();
+            if(firstCardClick.DoesPair(card))
+            {
+                ///Ganhar card
+            }
+            else
+            {
+
+                firstCardClick = null;
+            }
+        }
         Debug.Log("Card clicked " + card.GetCardId());
     }
 }
